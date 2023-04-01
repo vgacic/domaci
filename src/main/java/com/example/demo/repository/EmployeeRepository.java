2c3efcd980fba420a969f806200982d259959f55
vgacic <vgacic02@gmail.com>
2023-04-01 10:31:01 +0200

    Dodavanje test klase

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.repository;
 
-public interface EmployeeRepository {
+import com.example.demo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
